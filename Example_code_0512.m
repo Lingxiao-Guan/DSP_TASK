6d359a91198ec4db3332bff8b07368d68866e9b6
Lingxiao-Guan <53000211+Lingxiao-Guan@users.noreply.github.com>
2022-05-12 16:28:49 +0800

    Add files via upload

--- Example_code_0512.m
+++ Example_code_0512.m
@@ -20,15 +20,16 @@ title('chi-2')
 %% Task 2:
 % functions use: pwelch
 %  fs = 500;
+figure(2)
  nfft = 10*fs;
  noverlap = 0.5*fs;
  window = hanning(fs);
-[Pxx f] = pwelch(Data(1,2000:end),window,noverlap,nfft,fs);
+[Pxx f] = pwelch(Data(2,2000:end),window,noverlap,nfft,fs);
 Pxx_dB = 10*log10(abs(Pxx));
 plot(f,Pxx_dB,linewidth=1);
 xlabel('frequency(Hz)')
 ylabel('PSD(dB)')
-title('chi-1')
+title('chi-2')
 xlim([1,90])
 
 %% Task 3:
@@ -37,6 +38,7 @@ xlim([1,90])
 % overlap_stft = 0.75*fs;
 % window_length = fs;
 % noverlap = fs;
+figure(3)
 specgram(Data(2,2000:end),fs,fs,fs,fs*0.75)
 xlabel('time(s)')
 ylabel('frequency(Hz)')
